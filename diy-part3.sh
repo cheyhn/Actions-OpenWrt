6b7bc72d0204e66fbf57da36fc09c8f06fa00817
CHEY <czlaga@outlook.com>
2026-03-29 13:01:42 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone https://github.com/jerrykuku/luci-theme-argon.git package
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
